1cf4a2298a35ff6e3e4914227f947467b5945c9e
Cloud User <cloud-user@spackdev.novalocal>
2019-01-15 11:39:38 +0000

    Fixes coarray global maximum exercise
    Random seed size, initialize maximum value to zero

--- caf-max/maxval.f90
+++ caf-max/maxval.f90
@@ -3,15 +3,19 @@ program global_max
   integer, parameter :: n_rand = 1000
   real, dimension(n_rand) :: x
   real :: local_max[*], glbl_max
-  integer :: me, im, im_maxval, seed(2)
+  integer :: me, im, im_maxval, seed_size
+  integer, allocatable :: seed(:)
+
+  call random_seed(size=seed_size)
+  allocate(seed(seed_size))
 
   me = this_image()
   seed = me
   call random_seed(put=seed)
   call random_number(x)
   local_max = maxval(x)
-  print *, me, local_max
+  write(*,'(I5,A,F10.7)')  me, ': ', local_max
+
   sync all
 ! TODO: find the global maximum across the images
 end program global_max
-
--- caf-max/solution/maxval.f90
+++ caf-max/solution/maxval.f90
@@ -3,24 +3,31 @@ program global_max
   integer, parameter :: n_rand = 1000
   real, dimension(n_rand) :: x
   real :: local_max[*], glbl_max
-  integer :: me, im, im_maxval, seed(2)
+  integer :: me, im, im_maxval, seed_size
+  integer, allocatable :: seed(:)
+
+  call random_seed(size=seed_size)
+  allocate(seed(seed_size))
 
   me = this_image()
   seed = me
   call random_seed(put=seed)
   call random_number(x)
   local_max = maxval(x)
-  print *, me, local_max
+  write(*,'(I5,A,F10.7)')  me, ': ', local_max
+
   sync all
+
   if (me == 1) then
+     glbl_max = 0.0
      do im = 1, num_images()
         if (local_max[im] > glbl_max) then
            glbl_max = local_max[im]
            im_maxval = im
         end if
      end do
-     write(*,*) 'Global maximum=', glbl_max, &
-          ' was found at Image', im_maxval
+     write(*,'(A,F10.7,A,I0)') 'Global maximum ', glbl_max, &
+          ' was found at Image ', im_maxval
   end if
-end program global_max
 
+end program global_max
